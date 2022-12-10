ddfd776d30c7f97b524e194cd55b4707cb707243
Mikhail Dudarev <119784305+Millental@users.noreply.github.com>
2022-12-10 20:15:39 +0300

    Update M_1_SQL_Codes.sql
    кумулятивный SQL файл по лабораторной из задания 2.3

--- DE-101/module02/M_1_SQL_Codes.sql
+++ DE-101/module02/M_1_SQL_Codes.sql
@@ -1,6 +1,42 @@
 -- Посчитаем различные метрики с использованием языка SQL
 -- предварительно загрузили в DBEAVER-POSTGRES-LOCALHOST  три файла, создали из них три таблицы
 -- ORDERS RETURNS PEOPLE
-1
-SELECT sum(sales) total
+--total sales
+SELECT SUM(sales) total
 FROM orders
+--total profit
+SELECT SUM(profit) total
+FROM orders
+--profit ratio
+SELECT SUM(profit)/SUM(sales) ratio 
+FROM orders
+--Profit per Order
+SELECT SUM(profit)/count(distinct order_id) PPO
+FROM orders
+--Sales per Customer
+select SUM(sales)/COUNT(distinct customer_id) SPC
+from orders
+-- Avg. Discount
+SELECT AVG(discount)
+from orders
+--Monthly Sales by Segment 
+select  segment, extract(month from cast(order_date as timestamp)) monthly, sum(sales) amount
+from orders o 
+group by segment, monthly
+order by segment, monthly
+--Monthly Sales by Product Category 
+select  category, extract(month from cast(order_date as timestamp)) monthly, sum(sales) amount
+from orders o 
+group by category, monthly
+order by category, monthly
+--Sales and Profit by Customer - отсортировал по прибыльности, заодно получил CUSTOMER RANKING
+select customer_name customer, sum(sales) amount, sum(profit) profit
+from orders o
+group by customer_name 
+order by profit desc
+--Sales per region, отсортировал по прибыльности
+select region, sum(sales) amount, sum(profit) profit
+from orders o
+group by region 
+order by profit desc
+
