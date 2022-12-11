c8c77cb7213a3dd90ba2ae9dbf4e50f12e0ef872
Mikhail Dudarev <119784305+Millental@users.noreply.github.com>
2022-12-11 12:54:17 +0300

    Update DM _creation.sql

--- DE-101/module02/DM _creation.sql
+++ DE-101/module02/DM _creation.sql
@@ -5,11 +5,11 @@
 CREATE TABLE public.calendar
 (
  order_date date NOT NULL,
- year       int NOT NULL,
- quarter    int NOT NULL,
- month      int NOT NULL,
- week       int NOT NULL,
- weekday    int NOT NULL,
+ year       int NULL,
+ quarter    int  NULL,
+ month      int  NULL,
+ week       int  NULL,
+ weekday    int  NULL,
  CONSTRAINT PK_1 PRIMARY KEY ( order_date )
 );
 -- сначала перенесем из материнской таблицы orders колонку "Order_date" - это наше некалькулируемое поле и первичный ключ в одном лице
@@ -23,3 +23,15 @@ set "year" = extract(year from cast(order_date as timestamp)),
 "week" = extract(week from cast(order_date as timestamp)),
 "quarter" = extract(quarter from cast(order_date as timestamp)),
 "weekday" = extract(isodow from cast(order_date as timestamp))
+
+-- ************************************** product
+
+CREATE TABLE product
+(
+ product_id   varchar(50) NOT NULL,
+ category     varchar(50) NOT NULL,
+ subcategory  varchar(50) NOT NULL,
+ product_name varchar(50) NOT NULL,
+ segment      varchar(50) NOT NULL,
+ CONSTRAINT PK_1 PRIMARY KEY ( product_id )
+);
